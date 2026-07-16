498d5b851a4f7f961bab957a79c40e273d2ea930
enzo torricella <enzo.torricella25@gmail.com>
2026-07-16 10:15:53 -0300

    Add W02 temple album assignment

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("#nav-menu");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open");
+    navigation.classList.toggle("open");
+
+    const isOpen = navigation.classList.contains("open");
+    menuButton.setAttribute(
+        "aria-label",
+        isOpen ? "Close navigation menu" : "Open navigation menu"
+    );
+});
